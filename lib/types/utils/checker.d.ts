8d8dbc4efeb7623d2fca1f5e1bea4b65a16a98d4
wiskewu <wiskewu@gmail.com>
2022-07-23 15:49:09 +0800

    Build & Release V0.0.5

---
+++ lib/types/utils/checker.d.ts
@@ -0,0 +1,2 @@
+import { type MarkdownDescriptor } from '../interface';
+export declare const blogInfoValidate: (info: MarkdownDescriptor, filePath: string) => boolean;
--- lib/types/utils/sort.d.ts
+++ lib/types/utils/sort.d.ts
@@ -1,6 +1,6 @@
 import type { Post } from '../interface';
 /**
  * 根据权重和最后更新时间排序
- * @param latestFirst true：日期越近，则排在越前
+ * @param latestFirst true：最后更新日期越接近当前，则排在越前
  */
 export declare const sortUnderWeightAndDate: (posts: Post[], latestFirst?: boolean) => Post[];
